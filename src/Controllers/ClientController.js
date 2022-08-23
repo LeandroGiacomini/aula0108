21c51db4af3216fcd29707d404c3a9b73147e59a
fonsecaa007 <gel.giacomini@gmail.com>
2022-08-23 12:26:22 -0300

    aula 23/08

--- src/Controllers/ClientController.js
+++ src/Controllers/ClientController.js
@@ -5,7 +5,6 @@ export class ClientController{
     static async listarTodos( req, res){
         try {
             const clientes = await Client.SelectAll()
-            console.log(clientes)
             return res.status(200).json(clientes)
         } 
         catch(error) 
@@ -15,11 +14,33 @@ export class ClientController{
         }
     }
 
-    static novoCliente(req, res){
-        const { nome , idade } = req.body;
-        const cliente = new Client(nome, idade).Insert();
-        return res.status(200).json({
-            msg : 'Cadastrado com sucesso!'
-        });
+    static async novoCliente(req, res){
+        try {
+            const {nome, idade, cpf} = req.body
+            const novoCliente = await new Client(nome, idade, cpf).Insert()
+            return res.status(200).json(novoCliente);
+
+        } 
+        catch (error) 
+        {
+            console.log ('error novocliente' + error)
+            res.status(500).json(error)
+        }
     }
+
+    static async altClient(req, res){
+        try {
+            const {nome, idade, cpf} = req.body
+            const { id } = req.params
+            const altCliente = await new Client(nome, idade, cpf,id).Update()
+            return res.status(200).json(altCliente);
+            
+        } 
+        catch (error) 
+        {
+            
+        }
+
+    }
+
 }
